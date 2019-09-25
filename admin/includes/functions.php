c0a6c2fea0227059b96e71bc971aedd751a7f187
lindy.chang <lnchang@cwb.gov.tw>
2019-09-25 17:15:09 +0800

    initial commit

---
+++ admin/includes/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+    function classAutoloader($class) {
+        $class = strtolower($class);
+        $the_path = "includes/{$class}.php";
+
+        if(is_file($the_path) && !class_exists($class)) {
+            include $the_path;
+        }
+    }
+
+    spl_autoload_register('classAutoloader');
+
+
+    function redirect($location){
+    	header("Location: {$location}");
+    }
+?>
